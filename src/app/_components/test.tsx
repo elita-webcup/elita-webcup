7ab7b78fc8f87d1f833fcd34c9df3f97e0a5a60c
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-02 20:12:28 +0300

    feat: test prisma

---
+++ src/app/_components/test.tsx
@@ -0,0 +1,15 @@
+"use server"
+import React from 'react';
+import {create, find} from "@/app/_service/test_service";
+
+const Test = () => {
+    // create()
+    const users = find()
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Test;
