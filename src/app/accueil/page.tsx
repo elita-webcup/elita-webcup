478919837fd4efb382598ee36463cf4e87f295c7
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-05-05 07:38:33 +0300

    first

--- src/app/accueil/page.tsx
+++ src/app/accueil/page.tsx
@@ -1,7 +1,6 @@
 import Acceuil from "@/components/accueil/Acceuil";
 
-// eslint-disable-next-line react/display-name
-export default function (){
+export default function Page(){
     return (
         <div className="bg-gradient-radial mt-0 pt-4 from-lime-950 to-black h-[100vh]">
             <Acceuil/>
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,45 @@
-import {handlers} from "@/lib/auth";
+import NextAuth , {AuthOptions ,} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export {handlers as GET  ,handlers as POST}
+
+export const authOptions = {
+    session: {
+        strategy: 'jwt'
+    },
+    callbacks: {
+        async signIn({user}) {
+            console.log(user);
+            return !!user;
+
+        },
+        async session( {session, token, user}) {
+            return session
+        }
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: true,
+    providers: [
+        CredentialsProvider({
+            id: "login" ,
+            type: "credentials" ,
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                try{
+                    return {
+                        id: "12563",
+                        username: "James Bond"
+                    }
+                } catch (e) {
+                    throw new Error(e.message);
+                }
+
+            }
+        })
+    ],
+}
+export const handler = NextAuth(authOptions as AuthOptions);
+export {handler as GET, handler as POST}
